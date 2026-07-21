9134581ed978cf8e3dd864ce5fefe901f193bb52
Misha Ilchyshyn <ilchyshynmuchailo@gmail.com>
2026-07-21 14:23:54 +0300

    Init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+      <main id='core-concepts'>
+        <h2>Core Concepts</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
