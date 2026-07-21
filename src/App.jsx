a09726b96d8fdb01313c581a9e46a933ce63a14a
Misha Ilchyshyn <ilchyshynmuchailo@gmail.com>
2026-07-21 16:37:05 +0300

    Show "Core Concepts" block using "CoreConcept" component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Header from "./Header";
+import CoreConcept from "./CoreConcept";
+
+import { CORE_CONCEPTS } from "./data";
 
 function App() {
   return (
     <div>
       <Header />
 
-      <main id='core-concepts'>
-        <h2>Core Concepts</h2>
+      <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+
+          <ul>
+            {CORE_CONCEPTS.map((item) => (<CoreConcept key={item.title} {...item} />))}
+          </ul>
+        </section>
       </main>
     </div>
   );
